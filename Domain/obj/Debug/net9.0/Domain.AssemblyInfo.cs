ec6c38c199e128ba8d4b6af8ac67c9a487ca2cbd
Isa94d-lab <isabelagalvis7809@gmail.com>
2025-06-04 08:20:52 -0500

    feat: :sparkles: Connection with GenericRepository
    Se agregaron archivos necesarios para la conexion entre entidades

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c0c956843406efe384641a87cad5c330b332ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e313f0c5dc127669df10d0908407d67a07580d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
